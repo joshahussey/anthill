08839c256a99ab483a227ed609ab264cb2e9fb1e
joshahussey <josh@nfld.com>
2023-06-19 17:54:47 +0200

    Chore: Update colours in description

--- lua/anthill/ui-config.lua
+++ lua/anthill/ui-config.lua
@@ -8,12 +8,12 @@ M.info_highlight_target_colour = "#ff0000"
 -- UI FUNCTIONS --
 function M.remove_colour_highlight(bufnr, line_start, line_end)
 	for line = line_start, line_end do
-		vim.api.nvim_buf_clear_highlight(bufnr, -1, line - 1, 0, -1)
+		vim.api.nvim_buf_clear_namespace(bufnr, -1, line - 1, line_end)
 	end
 end
 
 function M.add_colour_highlight(bufnr, line_start, line_end)
-	vim.api.nvim_buf_add_highlight(bufnr, -1, M.info_highlight_group_name, line_start - 1, 0, -1)
+	vim.api.nvim_buf_add_highlight(bufnr, -1, M.info_highlight_target_name, line_start - 1, 0, -1)
 	for line = line_start + 1, line_end do
 		vim.api.nvim_buf_add_highlight(bufnr, -1, M.info_highlight_group_name, line - 1, 0, -1)
 	end
