1194c6a8203aa3b232b3db00a3810877586d9600
joshahussey <josh@nfld.com>
2023-06-19 17:45:10 +0200

    Chore: Update colours in description

--- lua/anthill/ui-config.lua
+++ lua/anthill/ui-config.lua
@@ -1,21 +1,24 @@
 local M = {}
 
-M.info_highlight_group_name = "info_group"
-M.info_highlight_text_colour = "#ff0000"
+M.info_highlight_group_name = "info_group_text_colour"
+M.info_highlight_target_name = "info_group_target_colour"
+M.info_highlight_text_colour = "#731d1d"
+M.info_highlight_target_colour = "#ff0000"
 
 -- UI FUNCTIONS --
 function M.remove_colour_highlight(bufnr, line_start, line_end)
-	-- Remove the highlight from the specified lines
-	vim.api.nvim_buf_clear_highlight(bufnr, -1, line_start - 1, 0, -1)
-	vim.api.nvim_buf_clear_highlight(bufnr, -1, line_end - 1, 0, -1)
+	for line = line_start, line_end do
+		vim.api.nvim_buf_clear_highlight(bufnr, -1, line - 1, 0, -1)
+	end
 end
-function M.add_colour_highlight(bufnr, line_start, line_end)
-	-- Add the highlight to the specified lines
-	vim.api.nvim_buf_add_highlight(bufnr, -1, M.info_highlight_group_name, line_start - 1, 0, -1)
-	vim.api.nvim_buf_add_highlight(bufnr, -1, M.info_highlight_group_name, line_end - 1, 0, -1)
 
-	-- Set the highlight group's colors
+function M.add_colour_highlight(bufnr, line_start, line_end)
+	vim.api.nvim_buf_add_highlight(bufnr, -1, M.info_highlight_group_name, line_start - 1, 0, -1)
+	for line = line_start + 1, line_end do
+		vim.api.nvim_buf_add_highlight(bufnr, -1, M.info_highlight_group_name, line - 1, 0, -1)
+	end
 	vim.cmd("highlight " .. M.info_highlight_group_name .. " guifg=" .. M.info_highlight_text_colour)
+	vim.cmd("highlight " .. M.info_highlight_target_name .. " guifg=" .. M.info_highlight_target_colour)
 end
 
 return M
